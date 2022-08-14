6a19f6731ce4fa6346f1eb9d8a2d5345c1698f90
ashikurrahmanbhuiyan <s2010376107@ru.ac.bd>
2022-08-14 18:56:07 +0600

    chageing

--- src/component/product/product.js
+++ src/component/product/product.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './product.css'
 
-const product = () => {
+const Product = (props) => {
+    const {img,name,seller,price,stock} =  props.product ;
     return (
-        <div>
-            <h1>this is product</h1>
+        <div className='product'>
+            <div>
+                <img src={img} alt=''/>
+            </div>
+            <div className='pro-name'>
+                <h5>{name}</h5>
+                <p><small>by : {seller}</small></p>
+                <p>${price}</p>
+                <p><small>only{stock} left in stock - order soon</small></p>
+                <button 
+                className='cart'
+                    onClick={()=> props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart}/> add to cart</button>
+            </div>
         </div>
     );
 };
 
-export default product;
+export default Product;
--- src/component/shop/shop.js
+++ src/component/shop/shop.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import fakeData from "../../fakeData";
 import './shop.css'
+import Product from '../product/product';
 
 const  Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const[products] =  useState(first10);
-    
+    // eslint-disable-next-line no-unused-vars
+    const[cart,setCart] = useState([]);
+
+    const handleAddProduct = (product)=>{
+console.log("product added",product);
+const newCart = [...cart,product];
+// eslint-disable-next-line no-const-assign
+setCart = newCart
+    }
 
     return (
         <div className='shop-container'>
             <div className='product-container'>
-                <ul>{products.map(element =>
-                    <li>{element.name}</li>
+                {products.map(element =>
+                    <Product
+                    product = {element}
+                    handleAddProduct={handleAddProduct}></Product>
                 )} 
-                </ul> 
             </div>
             <div className='cart-container'>
                 <h3>this is cart</h3>
+                <h5>Order Summery: {cart.length}</h5>
             </div>
         </div>
     );
